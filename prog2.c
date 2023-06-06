257bd56a0995c1d9d796b86ea4d2e7ba5e10ce57
nicecomputers123 <52811031+nicecomputers123@users.noreply.github.com>
2023-06-06 09:21:24 -0700

    Create prog2.c

---
+++ prog2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	printf("nice computer education\n");
+	printf("Main road, Opp: Town Hall\n");
+	printf("VRC CENTER\n");
+	printf("Nellore");
+  printf("contact : 8328274572");
+	return 0;
+}
