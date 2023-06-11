c10e636f6b565c2545e5b30fe7d594c1e67cdb5e
nicecomputereducation <52811031+nicecomputereducation@users.noreply.github.com>
2023-06-11 08:14:49 -0700

    Update prog6.c

--- prog6.c
+++ prog6.c
@@ -1,1 +1,10 @@
-
+#include<stdio.h>
+main()
+{
+  fir=30;
+  sec=10;
+  printf("The addition is %d\n",fir+sec)
+  printf("The subtraction is %d\n",fir-sec)
+  printf("The multiplication is %d\n",fir*sec)
+  printf("The division  is %d\n",fir/sec)
+  }
