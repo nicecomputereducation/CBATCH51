6fa9e3ba8f3d5f8010f95b83a326bc6c95cf7afa
nicecomputereducation <52811031+nicecomputereducation@users.noreply.github.com>
2023-06-11 08:18:04 -0700

    Create prog11.c

---
+++ prog11.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int first,second,sum;
+	printf("Oka number ivvu:");
+	scanf("%i",&first);
+	printf("Inko number ivvu:");
+	scanf("%i",&second);
+	sum=first+second;
+	printf("Addition %i",sum);
+	return 0;
+}
