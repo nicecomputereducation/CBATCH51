3198deda285b027547e6022d97326f7eb224f312
nicecomputereducation <52811031+nicecomputereducation@users.noreply.github.com>
2023-06-11 09:05:13 -0700

    Create progif43.c

---
+++ progif43.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int n,gr;
+	printf("Enter any natural number:");
+	scanf("%i",&n);
+	if(n%9==0)
+		gr=9;
+	else
+		gr=n%9;
+	printf("Generic root %i",gr);
+	return 0;
+}
