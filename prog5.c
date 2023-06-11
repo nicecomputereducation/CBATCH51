7c92df77a7731716d5b3d76a26fbc54c410e4045
nicecomputereducation <52811031+nicecomputereducation@users.noreply.github.com>
2023-06-11 08:10:28 -0700

    Create prog5.c

---
+++ prog5.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int len,bre,area,peri;
+	len=40;
+	bre=30;
+	area=len*bre;
+	peri=2*(len+bre);
+	printf("Area %i\n",area);
+	printf("Perimeter %i",peri);
+	return 0;
+}
