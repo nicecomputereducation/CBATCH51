73ae6a5bdb7362fe265b4e52086386b5a36b878a
nicecomputereducation <52811031+nicecomputereducation@users.noreply.github.com>
2023-06-29 17:44:40 -0700

    Create prog95.c

---
+++ prog95.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int i,n,fact;
+	printf("Enter any natural number:");
+	scanf("%i",&n);
+	fact=1;
+	i=1;
+	while(i<=n)
+	{
+		fact=fact*i;
+		i=i+1;
+	}
+	printf("factorial  %i",fact);
+	return 0;
+}
