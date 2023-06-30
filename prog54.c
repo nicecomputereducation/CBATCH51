75b7678b4895c810d4077bbd6d43a8440969438b
nicecomputereducation <52811031+nicecomputereducation@users.noreply.github.com>
2023-06-29 17:13:42 -0700

    Create prog54.c

---
+++ prog54.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int mat,phy,che;
+	printf("Enter marks in maths, physics and chemistry\n");
+	scanf("%i%i%i",&mat,&phy,&che);
+	if(mat>=35 && phy>=35 && che>=35)
+		printf("pass");
+	else
+		printf("failed");
+	
+	return 0;
+}
