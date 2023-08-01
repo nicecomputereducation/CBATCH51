36da2b3f08147a9927534b503166db4f055a7975
bagavath sirs nicecomputereducation <52811031+nicecomputereducation@users.noreply.github.com>
2023-07-31 18:57:00 -0700

    Create prog184.c

---
+++ prog184.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void address();
+int main()
+{
+	address();
+	return 0;	
+}
+void address()
+{
+	printf("nice computer education\n");
+	printf("Opp: Town Hall, VRC\n");
+	printf("Nellore");
+}
