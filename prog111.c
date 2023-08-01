77717e6688ff914845b167d5fc1a3fbfefe1e910
bagavath sirs nicecomputereducation <52811031+nicecomputereducation@users.noreply.github.com>
2023-07-31 18:13:45 -0700

    Create prog111.c

---
+++ prog111.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int n,min;
+	printf("Enter any natural number:");
+	scanf("%i",&n);
+	for(min=n%10;n!=0;n=n/10)
+	{
+		if(n%10<min)
+			min=n%10;
+	}
+	printf("The minimum digit %i",min);
+	return 0;
+}
