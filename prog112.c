d8e0c539f384b172b35557ae21febad1689de27f
bagavath sirs nicecomputereducation <52811031+nicecomputereducation@users.noreply.github.com>
2023-07-31 18:14:11 -0700

    Create prog112.c

---
+++ prog112.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int n,max;
+	printf("Enter any natural number:");
+	scanf("%i",&n);
+	for(max=n%10;n!=0;n=n/10)
+	{
+		if(n%10>max)
+			max=n%10;
+	}
+	printf("The maximum digit %i",max);
+	return 0;
+}
